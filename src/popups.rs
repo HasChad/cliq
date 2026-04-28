1334e005c1fa455fb1ac2541a7d228347ea5565a
HasChad <troll.yahya@gmail.com>
2026-04-28 15:08:41 +0300

    - cleaned and fixed all popups

--- src/popups.rs
+++ src/popups.rs
@@ -1,9 +1,9 @@
 use ratatui::{
     Frame,
     layout::{Alignment, Constraint, Flex, Layout, Rect},
-    style::{Color, Style, Stylize},
-    text::{Line, Span, Text},
-    widgets::{Block, Borders, Clear, Paragraph, TitlePosition, Wrap},
+    style::{Color, Stylize},
+    text::{Line, Text},
+    widgets::{Block, Borders, Clear, Paragraph, Wrap},
 };
 
 use crate::ai_logic::Message;
@@ -18,77 +18,67 @@ fn popup_area(area: Rect, length_x: u16, length_y: u16) -> Rect {
 
 pub fn popup_welcome(frame: &mut Frame) {
     let lines = vec![
-        Line::from(Span::styled(
-            "Welcome to Cliq",
-            Style::default().fg(Color::Yellow),
-        ))
-        .alignment(Alignment::Center),
-        Line::from(Span::styled(
-            "────────────────────────────────────",
-            Style::default().fg(Color::Yellow),
-        ))
-        .alignment(Alignment::Center),
-        Line::from(Span::styled(
-            "- Type '/help' for available commands",
-            Style::default().fg(Color::Blue),
-        )),
-        Line::from(Span::styled(
-            "- Press 'q' to close pop-ups",
-            Style::default().fg(Color::Blue),
-        )),
+        Line::default()
+            .spans(["Welcome to Cliq"])
+            .fg(Color::Yellow)
+            .alignment(Alignment::Center),
+        Line::default()
+            .spans(["────────────────────────────────────"])
+            .fg(Color::Yellow)
+            .alignment(Alignment::Center),
+        Line::default()
+            .spans(["- Type '/help' for available commands"])
+            .fg(Color::Blue),
+        Line::default()
+            .spans(["- Press 'q' to close pop-ups"])
+            .fg(Color::Blue),
     ];
+
     let text = Text::from(lines);
 
     let area = popup_area(frame.area(), 40, 6);
     frame.render_widget(Clear, area);
     frame.render_widget(
-        Paragraph::new(text).block(
-            Block::new()
-                .bold()
-                .fg(Color::Green)
-                .borders(Borders::ALL)
-                .title("")
-                .title_position(TitlePosition::Top),
-        ),
+        Paragraph::new(text).block(Block::new().fg(Color::Green).borders(Borders::ALL)),
         area,
     );
 }
 
 pub fn popup_help(frame: &mut Frame) {
     let lines = vec![
-        Line::from(Span::styled(
-            "Available Commands:",
-            Style::default().fg(Color::Yellow),
-        )),
-        Line::from(Span::styled(
-            " /exit | /quit  - Quit",
-            Style::default().fg(Color::Blue),
-        )),
-        Line::from(Span::styled(
-            " /help          - Show this help message",
-            Style::default().fg(Color::Blue),
-        )),
-        Line::from(Span::styled(
-            " /status        - Show current conversation status",
-            Style::default().fg(Color::Blue),
-        )),
-        Line::from(Span::styled(
-            " /clear         - Clear chat history",
-            Style::default().fg(Color::Blue),
-        )),
+        Line::default()
+            .spans(["Available Commands"])
+            .fg(Color::Yellow)
+            .alignment(Alignment::Center),
+        Line::default()
+            .spans(["────────────────────"])
+            .fg(Color::Yellow)
+            .alignment(Alignment::Center),
+        Line::default()
+            .spans(["/exit | /quit  - Quit"])
+            .fg(Color::Blue),
+        Line::default()
+            .spans(["/help          - Show this help message"])
+            .fg(Color::Blue),
+        Line::default()
+            .spans(["/status        - Show current conversation status"])
+            .fg(Color::Blue),
+        Line::default()
+            .spans(["/clear         - Clear chat history"])
+            .fg(Color::Blue),
     ];
+
+    let p_height = lines.len() as u16 + 2;
     let text = Text::from(lines);
 
-    let area = popup_area(frame.area(), 52, 7);
+    let area = popup_area(frame.area(), 51, p_height);
     frame.render_widget(Clear, area);
     frame.render_widget(
         Paragraph::new(text).block(
             Block::new()
-                .bold()
                 .fg(Color::Green)
                 .borders(Borders::ALL)
-                .title(" Help ")
-                .title_position(TitlePosition::Top),
+                .title(" Help "),
         ),
         area,
     );
@@ -99,108 +89,92 @@ pub fn popup_status(frame: &mut Frame, messages: &[Message]) {
     let assistant_messages = messages.iter().filter(|m| m.role == "assistant").count();
 
     let lines = vec![
-        Line::from(Span::styled(
-            "Conversation Status:",
-            Style::default().fg(Color::Yellow),
-        )),
-        Line::from(Span::styled(
-            format!("- Messages in history: {}", messages.len() - 1,),
-            Style::default().fg(Color::Blue),
-        )),
-        Line::from(Span::styled(
-            format!("- User messages: {}", user_messages),
-            Style::default().fg(Color::Blue),
-        )),
-        Line::from(Span::styled(
-            format!("- AI responses: {}", assistant_messages),
-            Style::default().fg(Color::Blue),
-        )),
+        Line::default()
+            .spans(["Conversation Status"])
+            .fg(Color::Yellow)
+            .alignment(Alignment::Center),
+        Line::default()
+            .spans(["────────────────────"])
+            .fg(Color::Yellow)
+            .alignment(Alignment::Center),
+        Line::default()
+            .spans([format!("- Messages in history: {}", messages.len() - 1,)])
+            .fg(Color::Blue),
+        Line::default()
+            .spans([format!("- User messages: {}", user_messages)])
+            .fg(Color::Blue),
+        Line::default()
+            .spans([format!("- AI responses: {}", assistant_messages)])
+            .fg(Color::Blue),
     ];
+
+    let p_height = lines.len() as u16 + 2;
     let text = Text::from(lines);
 
-    let area = popup_area(frame.area(), 35, 6);
+    let area = popup_area(frame.area(), 35, p_height);
     frame.render_widget(Clear, area);
     frame.render_widget(
         Paragraph::new(text).block(
             Block::new()
-                .bold()
                 .fg(Color::Green)
                 .borders(Borders::ALL)
-                .title(" Status ")
-                .title_position(TitlePosition::Top),
+                .title(" Status "),
         ),
         area,
     );
 }
 
 pub fn popup_sending_message(frame: &mut Frame) {
-    let lines = vec![Line::from(Span::styled(
-        " Sending message...",
-        Style::default().fg(Color::Yellow),
-    ))];
-    let text = Text::from(lines);
+    let line = Line::default()
+        .spans([" Sending message..."])
+        .fg(Color::Yellow);
+
+    let text = Text::from(line);
 
     let area = popup_area(frame.area(), 22, 3);
     frame.render_widget(Clear, area);
     frame.render_widget(
-        Paragraph::new(text).block(
-            Block::new()
-                .bold()
-                .fg(Color::Blue)
-                .borders(Borders::ALL)
-                .title("")
-                .title_position(TitlePosition::Top),
-        ),
+        Paragraph::new(text).block(Block::new().fg(Color::Blue).borders(Borders::ALL)),
         area,
     );
 }
 
 pub fn popup_quit(frame: &mut Frame) {
     let lines = vec![
-        Line::from(Span::styled(
-            " Are you sure want to quit?",
-            Style::default().fg(Color::Yellow),
-        )),
-        Line::from(Span::styled("", Style::default())),
-        Line::from(Span::styled(
-            "         ESC: Quit",
-            Style::default().fg(Color::Blue),
-        )),
+        Line::default()
+            .spans(["Are you sure want to quit?"])
+            .fg(Color::Yellow)
+            .alignment(Alignment::Center),
+        Line::default(),
+        Line::default()
+            .spans(["ESC: Quit"])
+            .fg(Color::Blue)
+            .alignment(Alignment::Center),
     ];
+
     let text = Text::from(lines);
 
     let area = popup_area(frame.area(), 30, 5);
     frame.render_widget(Clear, area);
     frame.render_widget(
-        Paragraph::new(text).block(
-            Block::new()
-                .bold()
-                .fg(Color::LightRed)
-                .borders(Borders::ALL)
-                .title("")
-                .title_position(TitlePosition::Top),
-        ),
+        Paragraph::new(text).block(Block::new().fg(Color::LightRed).borders(Borders::ALL)),
         area,
     );
 }
 
 pub fn popup_error(frame: &mut Frame, error_msg: &str) {
-    let lines = vec![Line::from(Span::styled(
-        error_msg,
-        Style::default().fg(Color::Yellow),
-    ))];
-    let text = Text::from(lines);
+    let line = Line::default().spans([error_msg]).fg(Color::Yellow);
+
+    let text = Text::from(line);
 
     let area = popup_area(frame.area(), 40, 7);
     frame.render_widget(Clear, area);
     frame.render_widget(
         Paragraph::new(text).wrap(Wrap { trim: true }).block(
             Block::new()
-                .bold()
-                .fg(Color::Green)
+                .fg(Color::LightRed)
                 .borders(Borders::ALL)
-                .title(" Error ")
-                .title_position(TitlePosition::Top),
+                .title(" Error "),
         ),
         area,
     );
